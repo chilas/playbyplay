3038ab66ab362b819a9d872a3a996abb87675132
Chilezie Reginald Unachukwu <iamchilas@gmail.com>
2016-10-16 09:28:03 +0100

    chore: StringToUpper

---
+++ StringToUpper.js
@@ -0,0 +1,21 @@
+"use strict";
+function StringToUpper(word) {
+    if (typeof word !== "string")
+        throw new Error("Can not convert non-string values");
+    var upperString;
+    for (var i = 0; i < word.length; i++) {
+        var charCode = void 0;
+        var currentCharCode = word.charCodeAt(i);
+        if ((currentCharCode < 65) ||
+            (currentCharCode > 122) ||
+            (currentCharCode >= 65 && currentCharCode <= 96)) {
+            charCode = word.charCodeAt(i);
+        }
+        else {
+            charCode = 65 + (word.charCodeAt(i) - 97);
+        }
+        upperString += String.fromCharCode(charCode);
+    }
+    return upperString;
+}
+exports.StringToUpper = StringToUpper;
---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+var StringToUpper_1 = require('./StringToUpper');
+var sentence = "I am Chilas!!!";
+var word = "@iamchilas";
+var char = "/";
+var number = "08055472282";
+var sentenceToUpper = StringToUpper_1.StringToUpper(sentence);
+var wordToUpper = StringToUpper_1.StringToUpper(word);
+var charToUpper = StringToUpper_1.StringToUpper(char);
+var numberToUpper = StringToUpper_1.StringToUpper(number);
+console.log("Sentence: " + sentenceToUpper);
+console.log("Word: " + wordToUpper);
+console.log("Char: " + charToUpper);
+console.log("Number: " + numberToUpper);
